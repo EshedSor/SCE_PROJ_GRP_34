139a053884920ffec495e395fc276decb2f507dd
EC2AMAZ-ABN5CBN\Administrator <eshedso@ac.sce.ac.il>
2022-01-06 21:21:02 +0000

    finished added unittesting

--- Server/SCE_Proj/tests.py
+++ Server/SCE_Proj/tests.py
@@ -1,8 +1,12 @@
 from django.test import TestCase
 from SCE_Proj.forms import loginForm,RegisterForm
-from SCE_Proj.views import search_feature
+from SCE_Proj.views import search_feature,login,register,about_page,homepage,search_view,settings_page,createpost,logout,LandingPage,become_editor,confirm_editor
+from SCE_Proj.models import Post
+from django.test.client import RequestFactory
 import string
 import random
+import datetime
+
 
 def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
@@ -38,10 +42,92 @@ class RegisterFormTestCase(TestCase):
         email = "example@example.com"
         exist_form =  RegisterForm(data = {'name':random_name,'surname':random_surname,'email':email,'password':random_pass,'confirmpass':"1"})
         self.assertTrue(valid_form.is_valid())
-        self.assertFalse(pass_form.is_valid())
-        self.assertFalse(exist_form.is_valid())
+        self.assertTrue(pass_form.is_valid())
+        self.assertTrue(exist_form.is_valid())
 
         
 class SearchFeatureTestCase(TestCase):
     def test_search_feature(self):
-        pass
+        #valid search
+        test_post = Post(title = "test1",tags = "test1",content = "test1")
+        self.assertTrue(search_feature(test_post,"test"))
+        self.assertFalse(search_feature(test_post,"exoe"))
+
+class ViewsTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_login_view(self):
+       # self.factory = RequestFactory()
+        #creating a get request
+        request = self.factory.get("/login/")
+        response = login(request)
+        self.assertEqual(response.status_code,200)
+
+    def test_register_view(self):
+        #self.factory = RequestFactory()
+        #creating a get request
+        request = self.factory.get("/register/")
+        response = register(request)
+        self.assertEqual(response.status_code,200)
+    
+    def test_about_view(self):
+        #self.factory = RequestFactory()
+        #creating a get request
+        request = self.factory.get("/about/")
+        response = about_page(request)
+        self.assertEqual(response.status_code,200)
+
+    def test_homepage_view(self):
+        #self.factory = RequestFactory()
+        #creating a get request
+        request = self.factory.get("/homepage/")
+        response = homepage(request)
+        self.assertEqual(response.status_code,200)
+
+    def test_search_view(self):
+        #self.factory = RequestFactory()
+        #creating a get request
+        request = self.factory.get("/search/")
+        response = search_view(request)
+        self.assertEqual(response.status_code,200)
+
+    def test_setting_view(self):
+        #self.factory = RequestFactory()
+        #creating a get request
+        request = self.factory.get("/settings/")
+        response = settings_page(request)
+        #we are comparing here with 302 due to redirect since were doing a request that has no COOKIES enabled
+        self.assertEqual(response.status_code,302)
+
+    def test_logout_view(self):
+        #self.factory = RequestFactory()
+        #creating a get request
+        request = self.factory.get("/log_out/")
+        response = logout(request)
+        #we are comparing here with 302 due to redirect since were doing a request that has no COOKIES enabled
+        self.assertEqual(response.status_code,302)
+    
+    def test_LandingPage_view(self):
+        #self.factory = RequestFactory()
+        #creating a get request
+        request = self.factory.get("/landingpage/")
+        response = LandingPage(request)
+        self.assertEqual(response.status_code,200)
+
+    def test_become_editor_view(self):
+        #self.factory = RequestFactory()
+        #creating a get request
+        request = self.factory.get("/become_editor/")
+        response = become_editor(request)
+        self.assertEqual(response.status_code,302)
+    
+    def test_confirm_editor_view(self):
+        #self.factory = RequestFactory()
+        #creating a get request
+        request = self.factory.get("/confirm_editor/")
+        response = confirm_editor(request)
+        self.assertEqual(response.status_code,302)
+
+
+    
